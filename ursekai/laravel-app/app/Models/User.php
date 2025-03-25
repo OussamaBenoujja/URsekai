b863094c11bea9c8ad897e2eb9eb262f0190c652
OussamaBenoujja <osama2code79@gmail.com>
2025-03-25 14:20:02 +0000

    Added C:\Users\Youcode\Desktop\laravelProjects\RedLine\ursekai\laravel-app\app\Models\User.php

--- ursekai/laravel-app/app/Models/User.php
+++ ursekai/laravel-app/app/Models/User.php
@@ -2,31 +2,43 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
+class User extends Authenticatable implements JWTSubject
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'display_name',
+        'avatar_url',
+        'bio',
+        'country',
+        'city',
+        'date_of_birth',
+        'theme_preference',
+        'account_level',
+        'experience_points',
+        'total_playtime_minutes',
+        'notification_preferences',
+        'privacy_settings',
     ];
 
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $hidden = [
         'password',
@@ -43,6 +55,29 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'notification_preferences' => 'array',
+            'privacy_settings' => 'array',
+            'date_of_birth' => 'date',
         ];
+    }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
