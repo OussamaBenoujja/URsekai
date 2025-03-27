77708aa91d5b28edabc88fd19c9cbddff829ce9e
OussamaBenoujja <osama2code79@gmail.com>
2025-03-27 10:19:31 +0000

    i have created all the endpoints related to the user and are fully working going to create user notification endpoints and friendship system

--- ursekai/laravel-app/app/Http/Controllers/API/UserProfileController.php
+++ ursekai/laravel-app/app/Http/Controllers/API/UserProfileController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use App\Traits\ApiResponser;
+use App\ApiResponser;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
